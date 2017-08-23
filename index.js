678115cd307796b90479e07dbe204a3472f705c2
isidoris <valentin.sorlin@hotmail.com>
2017-08-23 15:02:21 +0200

    aff list and function

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const mustache = require('mustache');
+const fs = require('fs');
+
+let db = ['toto', 'tata', 'titi', 'tutu', 'john'];
 
 let app = express();
 
-app.use(express.static("public"));
+let count = 0;
 
-app.get("/test", function(req, res) {
-    let str = mustache.render("Hello {{name}}!!! You are awesome!", {
-        name: "val"
-    })
-    res.send(str)
+function counter() {
+    return count++;
+}
+app.get("/", function(req, res) {
+    res.render("index", {
+        names: db,
+        rand: db[Math.round(Math.random() * (db.length - 0.5))],
+        counter: counter()
+    });
 });
 
+app.engine('html', function(filePath, options, callback) {
+    fs.readFile(filePath, function(err, content) {
+        if (err) {
+            return callback(err);
+        }
+        let str = mustache.render(content.toString(), options);
+        return callback(null, str);
+    });
+});
+
+app.set('views', './template');
+app.set('view engine', 'html');
+
+app.use(express.static("public"));
+
 app.listen(8080, function() {
-    console.log('Server listening on port 80...');
+    console.log('Server listening on port 8080...');
 });
